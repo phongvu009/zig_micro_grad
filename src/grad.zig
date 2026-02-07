da1d01ae7902948702aeacc674fb05dbb48b866c
phong vu <kimphong90@hotmail.com>
2026-02-07 14:54:43 -0600

    add addition math operation with test

--- src/grad.zig
+++ src/grad.zig
@@ -43,3 +43,30 @@ pub const Value = struct {
         std.debug.print("Value(label={?s}, value={}, grad={})", .{ self.label, self.value, self.grad });
     }
 };
+
+pub fn add(allocator: *const std.mem.Allocator, v1: *Value, v2: *Value) !*Value {
+    return try Value.init(allocator, v1.value + v2.value, "+");
+}
+
+test "test simple addition" {
+    //use special testing allocator
+    const allocator = &std.testing.allocator;
+
+    //create instance value
+    const a = try Value.init(allocator, 10.0, "a");
+    const b = try Value.init(allocator, 5.0, "b");
+    // perform addition
+    const c = try add(allocator, a, b);
+
+    //try return void
+    std.debug.print("Result is c label as {s} value is {d}\n", .{ c.label.?, c.value });
+
+    try std.testing.expectEqual(@as(f64, 15.0), c.value);
+
+    //clean-up
+    std.log.info("Attemptin to deinit", .{});
+    c.deinit();
+    a.deinit();
+    b.deinit();
+    std.log.info("Succes", .{});
+}
